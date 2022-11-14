5fdcb2eab845d47f914cf8e5eca5635c00b3cac3
Ubuntu <ubuntu@ip-172-31-18-46.us-east-2.compute.internal>
2022-11-14 10:37:57 +0000

    master update

--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -3,6 +3,8 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import https from "https";
 import react from "@vitejs/plugin-react";
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 if (
   process.env.npm_lifecycle_event === "build" &&
--- web/index.js
+++ web/index.js
@@ -12,6 +12,8 @@ import productCreator from "./helpers/product-creator.js";
 import redirectToAuth from "./helpers/redirect-to-auth.js";
 import { BillingInterval } from "./helpers/ensure-billing.js";
 import { AppInstallations } from "./app_installations.js";
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 const USE_ONLINE_TOKENS = false;
 
